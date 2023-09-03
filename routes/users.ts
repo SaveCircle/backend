2a8e28084699fb9c18f602bbd91b34eb99391f89
exploitenomah <iksploiting@gmail.com>
2023-09-03 12:29:36 +0100

    create auth routes

--- routes/users.ts
+++ routes/users.ts
@@ -1,10 +1,15 @@
 // @deno-types="npm:@types/express"
 import { Router } from "https://esm.sh/express@4.18.2"
 import { routeTryCatcher } from "../utils/routes.utils.ts"
-import { createNewUser } from "../controllers/user.ts"
+import { handlePostToUsers, handleGetByIdToUsers } from "../controllers/user.ts"
 import { jsonResponseSender } from "../middleware/routes.ts"
 
 const usersRouter = Router()
 
-usersRouter.get("/", routeTryCatcher(createNewUser), jsonResponseSender)
+usersRouter.post("/", routeTryCatcher(handlePostToUsers), jsonResponseSender)
+usersRouter.get(
+  "/:id",
+  routeTryCatcher(handleGetByIdToUsers),
+  jsonResponseSender
+)
 export default usersRouter
