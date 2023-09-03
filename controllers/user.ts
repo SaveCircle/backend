b7e4d201d942c169fc626b8b9f78e7868bc80e7f
exploitenomah <iksploiting@gmail.com>
2023-09-03 12:27:53 +0100

    create login and signup handlers

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,13 +1,81 @@
 import { RouteHandler } from "../types/routes.types.ts"
-import User from "../models/user.ts"
+import {
+  getUserByEmail,
+  regularUserSignup,
+  googleUserSignup,
+  loginUser,
+  generateEmailVerificationToken,
+  sendUserVerificationEmail,
+  getUserById,
+  verifyUser,
+  loginUserWithGoogle,
+} from "../utils/user.utils.ts"
+import env from "../deno.env.ts"
+import { generateResponse } from "../utils/routes.utils.ts"
 
-export const createNewUser: RouteHandler = (req, _res, next) => {
-  console.log(User, "ajfsjjkfd")
-  req.response = {
-    body: {
-      hello: "world",
-    },
-    status: 300,
+export const handlePostToUsers: RouteHandler = async (req, res, next) => {
+  if (!req.body.email) {
+    req.response = generateResponse({ message: "Not allowed" }, 401)
+    next()
   }
+  const user = await getUserByEmail(req.body.email)
+  if (!user) {
+    const { password } = req.body
+    if (password !== undefined) {
+      return regularUserSignup(req, res, next)
+    } else {
+      return googleUserSignup(req, res, next)
+    }
+  } else {
+    const { firstName, lastName, clientId, nbf, iss, exp } = req.body
+    if (!firstName || !lastName) {
+      if (!user.emailVerified) {
+        const vrfToken = generateEmailVerificationToken()
+        await sendUserVerificationEmail(user, vrfToken)
+        req.response = generateResponse({ email: "verify email" }, 200)
+        next()
+      } else {
+        req.user = user
+        if (req.body.password && user.accountType === "normal") {
+          return loginUser(req, res, next)
+        } else if (
+          user.accountType === "google" &&
+          clientId &&
+          clientId === env.get("GOOGLE_CLIENT_ID") &&
+          nbf &&
+          iss &&
+          exp &&
+          Math.sign(Date.now() - nbf * 1000) === 1 &&
+          Date.now() < exp * 1000 &&
+          iss === env.get("GOOGLE_ISS")
+        ) {
+          return loginUserWithGoogle(req, res, next)
+        } else {
+          req.response = generateResponse(
+            { message: "Invalid credentials" },
+            400
+          )
+          next()
+        }
+      }
+    } else {
+      req.response = generateResponse(
+        { message: "Email address is already in use" },
+        400
+      )
+      next()
+    }
+  }
+}
+
+export const handleGetByIdToUsers: RouteHandler = async (req, res, next) => {
+  const userId = req.params.id
+  req.user = await getUserById(userId)
+  if (!req.user) {
+    req.response = generateResponse({ message: "User not found!" }, 404)
+    next()
+  }
+  if (req.query["vrf-token"]) return await verifyUser(req, res, next)
+  req.response = generateResponse({ user: req.user }, 200)
   next()
 }
