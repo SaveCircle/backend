a8831a8b29c67d0d1dabb479a2be45aee8686b2d
exploitenomah <iksploiting@gmail.com>
2023-09-03 12:29:13 +0100

    create user model

--- models/user.ts
+++ models/user.ts
@@ -1,18 +1,32 @@
-// import { dango } from "https://deno.land/x/dangodb@v1.0.2/mod.ts"
+import { Bson } from "https://deno.land/x/mongo@v0.32.0/mod.ts"
+import db from "../mongodb/db.ts"
 
-// const userSchema = dango.schema({
-//   // A property accepts the below options. Only type is required and must be a valid selection.
-//   name: {
-//     type: "string", // Valid data types listed below.
-//     required: true, // A boolean to indicate if the inserted property must have a value specified. Defaults to false.
-//     unique: true, // A boolean to indicate if the inserted property much be unique in its value in the collection. Defaults to false.
-//     default: "T-Rex", // A value to default to if none specified and required = false. Defaults to null.
-//     validator: null, // A user provided validation function that must return true with the casted value as input for the data to pass schema validation. Defaults to null.
-//   },
-//   age: "number", // A property can also accept a schema value with only a type indicated.
-// })
-import { model, Schema } from "npm:mongoose@^6.7"
+export type googleAccount = "google"
+export type normalAccount = "normal"
 
-const userSchema = new Schema({})
+export type UserSchema = {
+  _id: Bson.ObjectId
+  password?: string
+  picture?: string
+  emailVerified?: boolean
+  firstName: string
+  lastName: string
+  email: string
+  emailVerificationToken?: string
+  passwordResetToken?: string
+  accountType: googleAccount | normalAccount
+}
 
-export default model("User", userSchema)
+const users = db.collection<UserSchema>("users")
+users.createIndexes({
+  indexes: [
+    {
+      key: {
+        email: -1,
+      },
+      name: "email",
+      unique: true,
+    },
+  ],
+})
+export default users
