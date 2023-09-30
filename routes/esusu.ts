0cc0a3899ad069786a879c1416c38eb6b00546d5
exploitenomah <iksploiting@gmail.com>
2023-09-30 03:42:01 +0100

    creaate esusu route

---
+++ routes/esusu.ts
@@ -0,0 +1,22 @@
+// @deno-types="npm:@types/express"
+import { Router } from "https://esm.sh/express@4.18.2"
+import { jsonResponseSender } from "../middleware/routes.ts"
+import {
+  authenticateUser,
+  checkForAuthenticatedUser,
+} from "../middleware/auth.ts"
+import {
+  createNewEsusu,
+  joinEsusu,
+  getSingleEsusu,
+  getManyEsusus,
+} from "../controllers/esusu.ts"
+
+const esusuRouter = Router()
+esusuRouter.use(authenticateUser, checkForAuthenticatedUser)
+esusuRouter.post("/", createNewEsusu, jsonResponseSender)
+esusuRouter.get("/", getManyEsusus, jsonResponseSender)
+esusuRouter.put("/:esusuId", joinEsusu, jsonResponseSender)
+esusuRouter.get("/:esusuId", getSingleEsusu, jsonResponseSender)
+
+export default esusuRouter
