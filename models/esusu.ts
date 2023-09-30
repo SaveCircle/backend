595d18d7b1e807340567533fed041c8c3e229999
exploitenomah <iksploiting@gmail.com>
2023-09-30 03:41:37 +0100

    create esusu model

---
+++ models/esusu.ts
@@ -0,0 +1,27 @@
+import { Bson } from "https://deno.land/x/mongo@v0.32.0/mod.ts"
+import db from "../mongodb/db.ts"
+
+export type googleAccount = "google"
+export type normalAccount = "normal"
+
+export type Cycle = "DAILY" | "WEEKLY" | "MONTHLY"
+export const cycle = ["DAILY", "WEEKLY", "MONTHLY"]
+export type esusuSchema = {
+  _id: Bson.ObjectId
+  creator: Bson.ObjectId
+  members: Bson.ObjectId[]
+  hasCollected: Bson.ObjectId[]
+  hasContributed: { round: number; contributors: Bson.ObjectId[] }[]
+  createdAt: Date
+  isCompleted: boolean
+  autoRepeat: boolean
+  contributionAmount: number
+  contributionFrequency: Cycle
+  isLocked: boolean
+  invitationKey: string
+  invitees: Bson.ObjectId[]
+}
+
+const esusus = db.collection<esusuSchema>("esusus")
+
+export default esusus
